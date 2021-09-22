ca4d61bb31f6c5eba217bcef56ba7a2bfff3ac0f
priyanshubansal <56711288+priyanahubansal@users.noreply.github.com>
2021-09-22 22:19:33 +0530

    second commit

--- src/Banner.js
+++ src/Banner.js
@@ -12,7 +12,7 @@ function Banner() {
                         </h1>
 
                         <p className="banner_description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-
+                        <button className="signupbutton">Sign Up</button>
                     </center>
                 </div>
             </div>
