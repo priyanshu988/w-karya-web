265a69177ce39e578d44754e21f01690bcbe3f47
priyanshubansal <56711288+priyanahubansal@users.noreply.github.com>
2021-09-30 13:04:23 +0530

    new look

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
 import React from 'react';
-import HomeScreen from './HomeScreen.js';
+import {Route} from "react-router-dom";
 import './App.css';
+import Navbar from './components/Navbar.js';
+import Home from './components/Home.js';
+import About from './components/About.js';
+import Contact from './components/Contact.js';
+import Login from './components/Login.js';
+import Signup from './components/Signup.js';
+import "bootstrap/dist/css/bootstrap.css";
+
+
 
 function App() {
   return (
     <div className="App">
-      <HomeScreen />
+      <Navbar />
+
+      <Route path="/">
+        <Home />
+      </Route>
+
+      <Route path="/about">
+        <About />
+      </Route>
+
+      <Route path="/contact">
+        <Contact />
+      </Route>
+
+      <Route path="/login">
+        <Login />
+      </Route>
+
+      <Route path="/signup">
+        <Signup />
+      </Route>
+
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,71 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
+import "./Navbar.css";
+import { NavLink } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <div>
+      <nav className="navbar sticky-top navbar-expand-lg navbar-light ">
+        <NavLink
+          className="navbar-brand"
+          to="#"
+          style={{ marginLeft: "20px", fontWeight: "bold", fontSize: "1.5em" }}
+        >
+          W-Karya
+        </NavLink>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item active">
+              <NavLink className="nav-link" to="/">
+                Home
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/about">
+                About Us
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/contact">
+                Contact Us
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/login">
+                Login
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                className="signup-button nav-link"
+                to="/signup"
+                style={{
+                  paddingLeft: "15px",
+                  color: "white",
+                  fontWeight: "bold",
+                }}
+              >
+                Sign Up
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
